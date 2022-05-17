b50c9e80cd39468e7402ba4ed80359cb64f232a0
ooha128 <goohashree@gmail.com>
2022-05-17 16:03:00 +0530

    final

--- lib/sales.dart
+++ lib/sales.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:rbk/homePage.dart';
 import 'inputform.dart';
@@ -64,40 +66,86 @@ class _salesState extends State<sales> {
   }
 
   Widget buildSalesCard(final d) {
-    return new Container(
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 4.0, bottom: 5.0),
-                child: Row(children: <Widget>[
-                  Icon(Icons.date_range),
+    List<String> s = d['fertilizers'];
+    return Card(
+        child: InkWell(
+      onTap: () => {
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Date: ${d['Date']}',
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            Text('Payment Id: ${d['Payment_id']}',
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            Container(
+              color: Colors.grey,
+              height: 300.0, // Change as per your requirement
+              width: 300.0, // Change as per your requirement
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: s.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                      title: Card(
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          s[index],
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        )),
+                  ));
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Close"),
+              ),
+            )
+          ],
+        )
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 4.0, bottom: 5.0),
+              child: Row(children: <Widget>[
+                Icon(Icons.date_range),
+                Spacer(),
+                Text(
+                  "${d['Date']}",
+                  style: new TextStyle(fontSize: 15.0),
+                ),
+              ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.payment),
                   Spacer(),
                   Text(
-                    "${d['Date']}",
+                    "${d['Payment_id']}",
                     style: new TextStyle(fontSize: 15.0),
                   ),
-                ]),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.payment),
-                    Spacer(),
-                    Text(
-                      "${d['Payment_id']}",
-                      style: new TextStyle(fontSize: 15.0),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
-    );
+    ));
   }
 }
